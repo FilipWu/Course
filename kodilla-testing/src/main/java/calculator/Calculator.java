35bd2ecdf567435d4d9fd57cdbe1376bb0b43984
FilipWu <84664246+FilipWu@users.noreply.github.com>
2025-02-09 21:02:42 +0100

    6.2 -added first testing

---
+++ kodilla-testing/src/main/java/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Calculator {
+
+    public int calculatorAdd(int a, int b) {
+        return a+b;
+    }
+    public int calculatorSubtract(int a, int b) {
+        return a-b;
+    }
+}
--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,37 @@
 package com.kodilla.testing;
 
+import calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        /*  SimpleUser user = new SimpleUser("Filip");
+            String result = user.getUsername();
+        if (result.equals("Filip")) {
+            System.out.println("Jest okej");
+        } else {
+            System.out.println("nie jest okej");
+        }
+        */
+
+        Calculator calculatorAdd = new Calculator();
+        Calculator calculatorSubtract = new Calculator();
+
+        int add = calculatorAdd.calculatorAdd(2,2);
+        int substract = calculatorSubtract.calculatorSubtract(2,2);
+
+        if (add == 4) {
+            System.out.println("Jest okej");
+        } else {
+            System.out.println("nie jest okej");
+        }
+        if (substract == 0){
+            System.out.println("Jest okej");
+        } else {
+            System.out.println("nie jest okej");
+        }
+
     }
 }

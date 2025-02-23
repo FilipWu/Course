ca684c6370529df2028f11ce575084aa921ebe68
FilipWu <84664246+FilipWu@users.noreply.github.com>
2025-02-23 22:28:15 +0100

    7.3 filtered forum users

--- kodilla-stream/src/main/java/com/kodilla/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/StreamMain.java
@@ -1,32 +1,82 @@
 package com.kodilla;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.iterate.NumbersGenerator;
-import com.kodilla.stream.lambda.*;
 
-import java.util.Locale;
+
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
+
+
+import java.time.LocalDate;
+import java.util.List;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
-        Processor processor = new Processor();
-        ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
-        processor.execute(executeSaySomething);
-/*
-        Executor codeToExecute = () -> System.out.println("This is an example text.");  // [7]
-        processor.execute(codeToExecute);
+/*        People.getList().stream()
+                .map(i ->i.toUpperCase())
+                .forEach(System.out::println);
 
-        processor.execute(() -> System.out.println("This is an example text."));
+        System.out.println();
 
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-        expressionExecutor.executeExpression(1,2, (a,b) -> a + b);
+        People.getList().stream()
+                .map(i -> i.toUpperCase())
+                .filter(n ->n.length() > 11)
+                .map(s -> s.substring(0, s.indexOf(" ") + 2) + ".")
+                .filter(s -> s.substring(0,1).equals("M"))
+                .forEach(System.out::println);*/
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        poemBeautifier.beautify("Hello World", poem -> "ABC" + poem + "ABC");
-        poemBeautifier.beautify("Filip", poem -> poem.toUpperCase());
-        poemBeautifier.beautify("Hello World", poem -> poem.toLowerCase());
-        poemBeautifier.beautify("Hello World   ", poem -> poem.trim());
+       // BookDirectory bookDirectory = new BookDirectory();
+
+     /*   bookDirectory.getList().stream()
+                .filter(n ->n.getYearOfPublication() > 2005)
+                .forEach(System.out::println);
 */
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
+        //lista
+
+       /* List<Book> theResultListOfBooks = bookDirectory.getList().stream()
+                .filter(n ->n.getYearOfPublication() > 2005)
+                .collect(Collectors.toList());
+        System.out.println("# elements: " + theResultListOfBooks.size());
+        theResultListOfBooks.stream()
+                .forEach(System.out::println);
+*/
+        //mapa
+      /*  Map<String,Book> theResultMapOfBooks = bookDirectory.getList().stream()
+                .filter(n ->n.getYearOfPublication() > 2005)
+                .collect(Collectors.toMap(Book::getSignature, book -> book));
+
+        System.out.println("# elements: " + theResultMapOfBooks.size());
+        theResultMapOfBooks.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);*/
+
+
+        Forum forum = new Forum();
+        Map<Integer,ForumUser> usersMap = forum.getList().stream()
+                .filter(n -> n.getGender() == 'M')
+                .filter(n -> n.getBirthday().isBefore(LocalDate.now().minusYears(20)) )
+                .filter(n -> n.getPosts() > 1)
+                .collect(Collectors.toMap(ForumUser::getId, user -> user));
+
+        usersMap.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }

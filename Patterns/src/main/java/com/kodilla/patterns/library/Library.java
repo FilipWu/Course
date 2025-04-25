80fcfd16918cb442c0473b522df25d58efdfe9d6
FilipWu <84664246+FilipWu@users.noreply.github.com>
2025-04-25 22:25:07 +0200

    11.2 Prototype copied Library

---
+++ Patterns/src/main/java/com/kodilla/patterns/library/Library.java
@@ -0,0 +1,45 @@
+package com.kodilla.patterns.library;
+
+import com.kodilla.patterns.prototype.Board;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class Library extends Prototype<Library> {
+
+    final String name;
+    Set<Book> books = new HashSet<>();
+
+    public Library(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    @Override
+    public String toString() {
+        return "Library{" +
+                "name='" + name + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    public Library shallowCopy() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public Library deepCopy() throws CloneNotSupportedException {
+        Library clonedLibrary = super.clone();
+        clonedLibrary.books = new HashSet<>();
+        for(Book book : books){
+            clonedLibrary.getBooks().add(book);
+        }
+        return clonedLibrary;
+    }
+}
